50df839e56b8bae44aa0dbde587bd820935f8fc4
David <artur99@Macbook-Pro-14-artur99.local>
2022-06-01 14:45:23 +0300

    Reorganised mainjs code

--- main.js
+++ main.js
@@ -1,31 +1,45 @@
 const http2 = require('http2');
-
 const fs = require('fs');
+
+const main_css_content = `
+    html {
+        background: red
+    }
+    /* alksjdlkajsdlkasjdlaksjd laksjd laskjd alskjd laksj dalksjd  *//* alksjdlkajsdlkasjdlaksjd laksjd laskjd alskjd laksj dalksjd  *//* alksjdlkajsdlkasjdlaksjd laksjd laskjd alskjd laksj dalksjd  *//* alksjdlkajsdlkasjdlaksjd laksjd laskjd alskjd laksj dalksjd  *//* alksjdlkajsdlkasjdlaksjd laksjd laskjd alskjd laksj dalksjd  *//* alksjdlkajsdlkasjdlaksjd laksjd laskjd alskjd laksj dalksjd  *//* alksjdlkajsdlkasjdlaksjd laksjd laskjd alskjd laksj dalksjd  */
+`
+const index_html_content = `
+    <!DOCTYPE html>
+    <!--<link rel="preload" href="main.css" as="style">-->
+    <h1>Hello World</h1>
+    <link rel="stylesheet" href="main.css" as="style">
+`
+
+
+
 
 const server = http2.createSecureServer({
     key: fs.readFileSync('./certs/localhost-privkey.pem'),
     cert: fs.readFileSync('./certs/localhost-cert.pem')
 }, (req, res) => {
-    console.log(`Requested: ${req.url}`)
+    // console.log(`Requested: ${req.url}`)
 
     if(req.url == '/favicon.ico') {
         console.log(`Received favicon.ico request: ${Date.now()/1000}`)
         res.writeHead(404);
-        res.end()
-        return
+        return res.end()
     }
+
     if(req.url == '/main.css') {
         console.log(`Received main.css request: ${Date.now()/1000}`)
         // console.log(req.headers)
-        let content = 'html{background: red} /* alksjdlkajsdlkasjdlaksjd laksjd laskjd alskjd laksj dalksjd  *//* alksjdlkajsdlkasjdlaksjd laksjd laskjd alskjd laksj dalksjd  *//* alksjdlkajsdlkasjdlaksjd laksjd laskjd alskjd laksj dalksjd  *//* alksjdlkajsdlkasjdlaksjd laksjd laskjd alskjd laksj dalksjd  *//* alksjdlkajsdlkasjdlaksjd laksjd laskjd alskjd laksj dalksjd  *//* alksjdlkajsdlkasjdlaksjd laksjd laskjd alskjd laksj dalksjd  *//* alksjdlkajsdlkasjdlaksjd laksjd laskjd alskjd laksj dalksjd  */'
+
+
         res.writeHead(200, {
-            'content-type': 'text/css',
+            'Content-Type': 'text/css',
             'Cache-Control': 'public, max-age=3600',
-            'expires': 'Wed, 02 Jun 2022 10:06:20 GMT',
-            'last-modified': 'Tue, 01 Sep 2015 22:51:45 GMT',
-            'content-length': content.length,
+            'content-length': main_css_content.length,
         });
-        setTimeout(_ => res.end(content), 50)
+        setTimeout(_ => res.end(main_css_content), 50)
         return
     }
 
@@ -34,42 +48,21 @@ const server = http2.createSecureServer({
 
     res.stream.additionalHeaders({
         ':status': '103',
-        // 'Link': '</main.css>; rel=preload; as=style',
-        // 'Link': '<main.css>; rel="preload"; as="style"',
+        'Link': '</main.css>; rel=preload; as=style',
     });
-
 
 
     res.writeHead(200, {
         'content-type': 'text/html',
-        // 'Link': '<https:///main.css>; rel="preload"; as="style"',
         // 'Link': '</main.css>; rel=preload; as=style',
     });
-
     res.flushHeaders()
 
-
-    setTimeout(() => {
-
-
-        res.end(`<!DOCTYPE html>
-        <!--<link rel="preload" href="main.css" as="style">-->
-        <h1>Hello World</h1>
-        <link rel="stylesheet" href="main.css" as="style">
-        `);
-    }, 5000)
+    setTimeout(_ => res.end(index_html_content), 3000)
 
 });
 
 // server.on('error', (err) => console.error(err));
 //
-// server.on('stream', (stream, headers) => {
-//
-//     stream.respond({
-//         'content-type': 'text/html; charset=utf-8',
-//         ':status': 200
-//     });
-//     stream.end('<h1>Hello World</h1>');
-// });
 
 server.listen(8443);
